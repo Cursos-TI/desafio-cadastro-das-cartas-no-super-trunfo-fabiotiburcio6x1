72a71c0e0c7b0384863baed3e92f4492456cb6c5
fabiotiburcio6x1 <fabioluistiburciosilva@gmail.com>
2025-02-27 00:21:34 +0000

    jogo de cartas fabin

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+int main(){
+    int população1, populaçaõ2, pontosturisticos1,  pontosturisticos2;
+    char estado1[20];
+    char estado2[20];
+    char codigoCarta1[6];
+    char codigoCarta2[6];
+    char nomeCidade1[10];
+    char nomeCidade2[10]; 
+    float população1;
+    float população2;
+    float area1;
+    float area2;
+    float PIB1;
+    float PIB2;
+
+    
+
+}
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
